6a198839733c63680a37cd47fba0a19f6b3fd7e5
Michal Olkiewicz <molkiewicz@sigma.ug.edu.pl>
2019-03-29 13:42:57 +0100

    Starting making testes for PhotoController.

---
+++ PhotoSharingApplication/PhotoSharingTests/PhotoControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using PhotoSharingApplication.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PhotoControllerTest
+{
+    [TestClass]
+    public class PhotoControllerTest
+    {
+        [TestMethod]
+    }
+}
